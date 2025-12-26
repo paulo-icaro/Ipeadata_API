8111caa153f6959d60b1e011d4a7cf59441fddaf
Paulo Icaro <pauloicaro38@gmail.com>
2025-12-25 22:13:52 -0300

    Expansão do tryCatch a toda a estrutura Data Extraction

--- Ipeadata_Query.R
+++ Ipeadata_Query.R
@@ -10,16 +10,16 @@
 # ============= #
 ipeadata_query = function(ipeadata_series_code, ipeadata_series_name, time_interval, source_github = TRUE){
   
-  # ----------------- #
-  # --- Libraries --- #
-  # ----------------- #
+  # ---------------------------------- #
+  # --- Source Auxiliary Functions --- #
+  # ---------------------------------- #
   if(source_github == TRUE){
-    tryCatch(expr = supressWarnings(source('https://raw.githubusercontent.com/paulo-icaro/Ipeadata_API/refs/heads/main/Ipeadata_API.R')),
+    tryCatch(expr = suppressWarnings(source('https://raw.githubusercontent.com/paulo-icaro/Ipeadata_API/refs/heads/main/Ipeadata_API.R')),
              error = function(e){message('Não foi possível acessar a função Ipeadata_API')})
     
     Sys.sleep(1.5)
     
-    tryCatch(expr = supressWarnings(source('https://raw.githubusercontent.com/paulo-icaro/Ipeadata_API/refs/heads/main/Ipeadata_URL.R')),
+    tryCatch(expr = suppressWarnings(source('https://raw.githubusercontent.com/paulo-icaro/Ipeadata_API/refs/heads/main/Ipeadata_URL.R')),
       error = function(e){message('Não foi possível acessar a função Ipeadata_URL')})
     
     Sys.sleep(1.5)
@@ -36,21 +36,27 @@ ipeadata_query = function(ipeadata_series_code, ipeadata_series_name, time_inter
   # ----------------------- #
   for(i in seq_along(ipeadata_series_code)){
     
-    # --- Extraction --- #
-    tryCatch(expr = {ipeadata_dataset_raw = ipeadata_api(url = ipeadata_url(series_cod = ipeadata_series_code[i]))},
-             error = function(e){stop('Uma ou mais funções não estão disponíveis, impedindo o código de ser executado. Verifique sua conexão e tente novamente ou importe de um diretório local.', call. = FALSE)})
-                      
-    # --- Data Manipulation --- #
-    ipeadata_dataset_raw = rawToChar(x = ipeadata_dataset_raw$content)
-    ipeadata_dataset_raw = fromJSON(ipeadata_dataset_raw, flatten = TRUE)$value
-    ipeadata_dataset_raw = ipeadata_dataset_raw[c(2,3)] %>% filter(substr(VALDATA, start = 1, stop = 4) %in% c(time_interval))
     
-    # --- Grouping Columns --- #
-    if(i == 1){ipeadata_dataset = ipeadata_dataset_raw}
-    else{ipeadata_dataset = leftjoin(x = ipeadata_dataset, y = ipeadata_dataset_raw, by = join_by('VALDATA' == 'VALDATA'))}
-    
-    # --- Headers --- #
-    if(i == length(ipeadata_series_name)){colnames(ipeadata_dataset) = c('data', ipeadata_series_name)}
+    tryCatch(expr = {
+      
+      # --- Extraction --- #
+      ipeadata_dataset_raw = ipeadata_api(url = ipeadata_url(series_cod = ipeadata_series_code[i]))
+      
+      # --- Data Manipulation --- #
+      ipeadata_dataset_raw = rawToChar(x = ipeadata_dataset_raw$content)
+      ipeadata_dataset_raw = fromJSON(ipeadata_dataset_raw, flatten = TRUE)$value
+      ipeadata_dataset_raw = ipeadata_dataset_raw[c(2,3)] %>% filter(substr(VALDATA, start = 1, stop = 4) %in% c(time_interval))
+      
+      # --- Grouping Columns --- #
+      if(i == 1){ipeadata_dataset = ipeadata_dataset_raw}
+      else{ipeadata_dataset = left_join(x = ipeadata_dataset, y = ipeadata_dataset_raw, by = join_by('VALDATA' == 'VALDATA'))}
+      
+      # --- Headers --- #
+      if(i == length(ipeadata_series_name)){colnames(ipeadata_dataset) = c('data', ipeadata_series_name)}
+      },
+      
+      error = function(e){stop('Uma ou mais funções não estão disponíveis ou não há conexão de internet. Verifique sua conexão ou importe as funções de um diretório local.', call. = FALSE)
+    })
   }
   
   # -------------- #    
