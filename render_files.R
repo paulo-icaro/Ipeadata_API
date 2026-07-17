58ee12fef225b462ce8efd3de1370b86f51b5abd
paulo-icaro <pauloicaro38@gmail.com>
2026-07-17 14:17:06 -0300

    Arquivo de renderização

---
+++ render_files.R
@@ -0,0 +1,30 @@
+# ==================== #
+# === RENDER FILES === #
+# ==================== #
+
+# --- Script by: Paulo Icaro --- #
+
+
+# ------------------- #
+# --- Bibliotecas --- #
+# ------------------- #
+library(quarto)
+
+
+# -------------------- #
+# --- Renderização --- #
+# -------------------- #
+files = c('readme_en_us.qmd', 'readme_pt_br.qmd')
+formats = c('pdf', 'gfm', 'html')
+
+for (i in seq_along(files)){
+  for (j in seq_along(formats)){
+    quarto_render(input = files[i], output_format = formats[j])
+  }
+}
+
+
+# --------------- #
+# --- Limpeza --- #
+# --------------- #
+rm(files, formats, i)
